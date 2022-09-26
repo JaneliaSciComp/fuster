d13d7a8e31b12a833203b36d5c50f1effaa536ae
Kristin Branson <bransonk@janelia.hhmi.org>
2022-09-26 13:37:58 -0400

    Updates to bjob calls to make parsing easier, allow sshhost to be specified.

--- get_bsub_job_status.m
+++ get_bsub_job_status.m
@@ -1,8 +1,9 @@
-function result = get_bsub_job_status(job_ids)
+function [result,bjobs_lines] = get_bsub_job_status(job_ids,varargin)
     % Possible results are {-1,0,+1}.
     %   -1 means errored out
     %    0 mean running or pending
     %   +1 means completed successfully
+    %   nan other 
     
     job_count = length(job_ids) ;
     result = nan(size(job_ids)) ;
@@ -17,24 +18,14 @@ function result = get_bsub_job_status(job_ids)
     end
     was_submitted = ~(was_run_locally | is_not_yet_submitted) ;
     submitted_job_ids = job_ids(was_submitted) ;
-    bjobs_lines = get_bjobs_lines(submitted_job_ids) ;
+    bjobs_lines = get_bjobs_lines(submitted_job_ids,varargin{:}) ;
     bjobs_line_index = 1 ;
     for job_index = 1 : job_count ,
         if ~was_submitted(job_index) ,
             continue ;
         end
         job_id = job_ids(job_index) ;
-        bjobs_line = bjobs_lines{job_index} ;
-        tokens = strsplit(bjobs_line) ;
-        if length(tokens)<3 ,
-            error('There was a problem submitting the bsub command %s.  Unable to parse output.  Output was: %s', bsub_command, stdout) ;
-        end
-        running_job_id_as_string = tokens{1} ;
-        running_job_id = str2double(running_job_id_as_string) ;
-        if running_job_id ~= job_id ,
-            error('The running job id (%d) doesn''t match the job id (%d)', running_job_id, job_id) ;
-        end
-        lsf_status = tokens{3} ;  % Should be string like 'DONE', 'EXIT', 'RUN', 'PEND', etc.
+        lsf_status = bjobs_lines{job_index} ; % Should be string like 'DONE', 'EXIT', 'RUN', 'PEND', etc.
         if isequal(lsf_status, 'DONE') ,
             running_job_status_code = +1 ;
         elseif isequal(lsf_status, 'EXIT') ,
@@ -44,7 +35,11 @@ function result = get_bsub_job_status(job_ids)
                isequal(lsf_status, 'SSUSP') || isequal(lsf_status, 'PSUSP') || isequal(lsf_status, 'USUSP'),
             running_job_status_code = 0 ;
         else
-            error('Unknown bjobs status string: %s', lsf_status) ;
+          if ~isempty(regexp(lsf_status,'not found','once')),
+            running_job_status_code = nan;
+          else
+            error('Unknown bjobs status string for job %d: %s', job_id,lsf_status) ;
+          end
         end
         result(job_index) = running_job_status_code ;
         bjobs_line_index = bjobs_line_index + 1 ;
--- get_single_bsub_job_status.m
+++ get_single_bsub_job_status.m
@@ -1,9 +1,14 @@
-function result = get_single_bsub_job_status(job_id)
+function [result,lsf_status] = get_single_bsub_job_status(job_id,sshhost)
     % Possible results are {-1,0,+1}.
     %   -1 means errored out
     %    0 mean running or pending
     %   +1 means completed successfully
-    
+
+    if nargin < 2,
+      sshhost = '';
+    end
+    lsf_status = '';
+
     if ~isfinite(job_id) ,
         % This means the job has not been submitted yet
         result = nan ;
@@ -14,21 +19,15 @@ function result = get_single_bsub_job_status(job_id)
         % This is a job that was run locally and errored
         result = -1 ;
     else
-        command_line = sprintf('bjobs %d', job_id) ;
+        command_line = sprintf('bjobs -o stat -noheader %d',job_id);
+        if ~isempty(sshhost),
+          command_line = sprintf('ssh -q %s "%s"',sshhost,command_line);
+        end
         [status, stdout] = system(command_line) ;
         if status ~= 0 ,
             error('There was a problem running the command %s.  The return code was %d', command_line, status) ;
         end
-        lines = strsplit(stdout, '\n') ;
-        if length(lines)<2 ,
-            error('There was a problem submitting the bsub command %s.  Unable to parse output.  Output was: %s', bsub_command, stdout) ;
-        end        
-        bjobs_line = lines{2} ;
-        tokens = strsplit(bjobs_line) ;
-        if length(tokens)<3 ,
-            error('There was a problem submitting the bsub command %s.  Unable to parse output.  Output was: %s', bsub_command, stdout) ;
-        end
-        lsf_status = tokens{3} ;  % Should be string like 'DONE', 'EXIT', 'RUN', 'PEND', etc.    
+        lsf_status = strtrim(stdout); % Should be string like 'DONE', 'EXIT', 'RUN', 'PEND', etc.    
         if isequal(lsf_status, 'DONE') ,
             result = +1 ;
         elseif isequal(lsf_status, 'EXIT') ,
